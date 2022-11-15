60a4edaa469d5b999efc7db63e516392f87a215a
sakhi <sakhi.anwari@hva.nl>
2022-11-15 03:04:02 +0100

    filter aangemaakt

---
+++ projectFolder/corendonmatchingsite/assets/js/main.js
@@ -0,0 +1,31 @@
+//filter van de matching+filter
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu li');
+
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        options.addEventListener('click', () => {
+            select.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            option.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.remove('active');
+        });
+    });
+});
+
+
