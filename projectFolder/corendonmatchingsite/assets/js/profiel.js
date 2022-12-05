f19cff4d500ca363911fc4dca8fc7d3bd6cabd24
Mohamed <mohamed.embaby@hva.nl>
2022-12-05 20:28:12 +0100

    met datebase connectie daarbij

--- projectFolder/corendonmatchingsite/assets/js/profiel.js
+++ projectFolder/corendonmatchingsite/assets/js/profiel.js
@@ -1,24 +1,22 @@
-// $(".toggle_btn").click(function(){
-//     $(this).toggleClass("active");
-//     $(".wrapper ul").toggleClass("active");
-//
-//     if($(".toggle_btn").hasClass("active")){
-//         $(".toggle_text").text("Show Less");
-//     }
-//     else{
-//         $(".toggle_text").text("Show More");
-//     }
-// });
+// Fys cloud datebase
+FYSCloud.API.queryDatabase(
+    "SELECT * FROM gebruiker WHERE email = 'emily56@email.com'"
+
+    // wat de datebase terug stuurt
+).then(function (data) {
+    console.log(data);
+// waarom het mis gaat
+}).catch(function (reason) {
+    console.log(reason);});
 
 // Popup stuff
-
 const serviceItems = document.querySelector(".service-items");
 const popup = document.querySelector(".popup-box")
 const popupCloseBtn = popup.querySelector(".popup-close-btn");
 const popupCloseIcon = popup.querySelector(".popup-close-icon");
 
-serviceItems.addEventListener("click",function(event){
-    if(event.target.tagName.toLowerCase() == "button"){
+serviceItems.addEventListener("click", function (event) {
+    if (event.target.tagName.toLowerCase() == "button") {
         console.log("Clicked")
         const item = document.querySelector(".popup_stuff");
         const h3 = item.querySelector("h3").innerHTML;
@@ -33,13 +31,15 @@ serviceItems.addEventListener("click",function(event){
 popupCloseBtn.addEventListener("click", popupBox);
 popupCloseIcon.addEventListener("click", popupBox);
 
-popup.addEventListener("click", function(event){
-    if(event.target == popup){
+popup.addEventListener("click", function (event) {
+    if (event.target == popup) {
         popupBox();
     }
 })
 
-function popupBox(){
+function popupBox() {
     popup.classList.toggle("open");
 }
+
+// Fys Cloud stuff
 
