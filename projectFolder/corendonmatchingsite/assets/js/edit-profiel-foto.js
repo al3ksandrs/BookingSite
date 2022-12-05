7e777c6090cd17ee4a2c06f420494649b8e6400e
Almaz Fkyerat <fkyeraa1@id.hva.nl>
2022-12-05 13:18:49 +0100

    update, nog niet klaar

---
+++ projectFolder/corendonmatchingsite/assets/js/edit-profiel-foto.js
@@ -0,0 +1,21 @@
+// FYSCloud.API.queryDatabase(
+//     "SELECT * FROM message"
+// ).then(function(data) {
+//     console.log(data);
+// }).catch(function(reason) {
+//     console.log(reason);
+// });
+
+alert ('Hello ');
+// const taskInput = document.getElementById('biografie-form');
+//
+// form.addEventListener('submit, runEvent');
+//
+// function runEvent (e){
+//     console.log(`EVENT TYPE: ${E.TYPE}`);
+//
+// //Get Input value
+// console.log(taskInput.value);
+//
+// e.preventDefault();
+// // }
