2660f1f90fb467c7e071b62a4e9bfa5e7dd79be6
Almaz Fkyerat <fkyeraa1@id.hva.nl>
2022-12-05 13:56:51 +0100

    update,

--- projectFolder/corendonmatchingsite/assets/js/edit-profiel-foto.js
+++ projectFolder/corendonmatchingsite/assets/js/edit-profiel-foto.js
@@ -1,13 +1,14 @@
 alert ('Hello ');
-// const taskInput = document.getElementById('biografie-form');
-//
-// form.addEventListener('submit, runEvent');
-//
-// function runEvent (e){
-//     console.log(`EVENT TYPE: ${E.TYPE}`);
-//
-// //Get Input value
-// console.log(taskInput.value);
-//
-// e.preventDefault();
-// // }
+const form = document.querySelector('form');
+const taskInput = document.getElementById('voornaam');
+
+form.addEventListener('submit', runEvent);
+
+function runEvent (e){
+    console.log(`EVENT TYPE: ${e.type}`);
+
+//Get Input value
+console.log(taskInput.value);
+
+e.preventDefault();
+}
