618948919d95beb7acf01ad3888a0d07e39747e7
Lars Uijlenhoet <lars.uijlenhoet@hva.nl>
2022-12-12 13:07:11 +0100

    Aanmaken profiel JS

--- projectFolder/corendonmatchingsite/assets/js/aanmaken-profiel.js
+++ projectFolder/corendonmatchingsite/assets/js/aanmaken-profiel.js
@@ -24,3 +24,49 @@ function gogo() {
                 console.log(reason);
             })
 }
+
+window.addEventListener("DOMContentLoaded", initialize);
+
+function initialize() {
+    initializeGebruiker();
+}
+
+function initializeGebruiker(){
+    document.querySelector("input").addEventListener("click", evt => submitGebruiker(evt))
+}
+
+function submitGebruiker(evt) {
+    evt.preventDefault();
+
+    let email = FYSCloud.Session.get("temp_email");
+    let wachtwoord = FYSCloud.Session.get("temp_pass")
+    let naam = document.querySelector("#naam").value;
+    let leeftijd = document.querySelector("#leeftijd").value;
+    let biografie = document.querySelector("#bio").value;
+
+    let gebruiker = new Gebruiker(null, email, wachtwoord, naam, leeftijd, biografie);
+    console.log(gebruiker);
+    insertGebruikerDb(gebruiker);
+}
+
+function insertGebruikerDb(gebruiker) {
+    FYSCloud.API.queryDatabase(
+        "INSERT INTO 'gebruiker' ('email', 'wachtwoord', 'naam', 'leeftijd', 'biografie') VALUES (?,?,?,?,?);",
+        [gebruiker.email,  gebruiker.wachtwoord, gebruiker.naam, gebruiker.leeftijd, gebruiker.biografie]
+    )
+        .then(response => {
+            Gebruiker.id = response.insertId;
+        })
+        .catch(function(reason) {
+            console.error(reason);
+        });
+}
+
+function Gebruiker(id, email, wachtwoord, naam, leeftijd, biografie) {
+    this.id = id;
+    this.email = email
+    this.wachtwoord = wachtwoord;
+    this.naam = naam;
+    this.leeftijd = leeftijd;
+    this.biografie = biografie;
+}
