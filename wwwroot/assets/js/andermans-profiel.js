b65a6b47a080f9d9730e5dae151860169afed985
Lars Uijlenhoet <lars.uijlenhoet@hva.nl>
2022-12-14 16:35:19 +0100

    Opnieuw reizen naar update

--- wwwroot/assets/js/andermans-profiel.js
+++ wwwroot/assets/js/andermans-profiel.js
@@ -1,7 +1,6 @@
 // Fys cloud datebase
 FYSCloud.API.queryDatabase(
     "SELECT * FROM gebruiker WHERE email = 'emily56@email.com'"
-
     // wat de datebase terug stuurt
 ).then(function (data) {
     console.log(data);
@@ -9,16 +8,45 @@ FYSCloud.API.queryDatabase(
 }).catch(function (reason) {
     console.log(reason);});
 
+    // Laden van de database gebruiker gegevens
+
+// Inhoud Stuff
+const bioInh = document.querySelector("#biografie_inhoud");
+const pfpInh = document.querySelector("#profielfoto");
+const prlInh = document.querySelector("#profielnaam");
+// removing the first character '?'
+const search = window.location.search;
+const userId = search.slice(1);
+console.log(userId);
+
+function age(dateString) {
+    let birth = new Date(dateString);
+    let now = new Date();
+    let beforeBirth = ((() => {birth.setDate(now.getDate());birth.setMonth(now.getMonth()); return birth.getTime()})() < birth.getTime()) ? 0 : 1;
+    return now.getFullYear() - birth.getFullYear() - beforeBirth;
+}
+
+FYSCloud.API.queryDatabase(
+    'SELECT * FROM gebruiker WHERE id = ?', [userId]
+).then(function (data){
+    bioInh.innerHTML = data[0].biografie
+    pfpInh.src = "/uploads/" + data[0].id + "." + data[0].fotoextensie
+    prlInh.innerHTML = data[0].naam + "         " + age(data[0].leeftijd)
+
+}).catch(function (reason){
+    console.log(reason)
+})
+
 
 // LIKE/MATCHING SYSTEEM
 
-function likeButtonClick(){
+function likeButtonClick(){             //kan nu const window.location.search gebruiken
     console.log("Huidige gebruiker: " + FYSCloud.Session.get("userId"));
 }
 
-const naam = document.querySelector("#naamtext");
+const naam = document.querySelector("#profielnaam");
 
-FYSCloud.API.queryDatabase(
+FYSCloud.API.queryDatabase(                 //kan nu const window.location.search gebruiken
     'SELECT * FROM gebruiker WHERE id = ?', [FYSCloud.Session.get("userId", "Not Found")]
 ).then(function (data){
     naam.innerHTML = data[0].naam
