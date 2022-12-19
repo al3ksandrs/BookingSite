8ee82a8946641d869d7868d35ef53496b4d9e172
Almaz Fkyerat <fkyeraa1@id.hva.nl>
2022-12-19 17:56:39 +0100

    update

--- wwwroot/assets/js/edit-profiel-foto.js
+++ wwwroot/assets/js/edit-profiel-foto.js
@@ -11,42 +11,42 @@
 //     console.log(reason);});
 
 //input profiel content
-const form = document.querySelector('.profielc-form,.biografie-form,.interesses-form,.interesses-activiteit');
-const taskInput = document.getElementById('voornaam');
-const taskInput1 = document.getElementById('leeftijd');
-const taskInput2 = document.getElementById('image');
-const taskInput3 = document.getElementById('geslacht');
-//input Biografie
-const taskInput4 = document.getElementById('biografie');
-// input interesses
-const taskInput5 = document.getElementById('gebruikersnaam');
-
-form.addEventListener('submit',runEvent);
-function runEvent(e){
-    console.log(`EVENT TYPE: ${e.type}`);
-
-//Get Input value
-console.log(taskInput.value,taskInput1.value,taskInput2.value,taskInput3.value,taskInput4,taskInput5.value);
-e.preventDefault();
-}
-
-//checkbox
-const cb = document.querySelector('#checkboxShopen,#checkboxZwemmen,#checkboxUitgaan');
-console.log(cb.checked); // false
-
+// const form = document.querySelector('.profielc-form,.biografie-form,.interesses-form,.interesses-activiteit');
+// const taskInput = document.getElementById('voornaam');
+// const taskInput1 = document.getElementById('leeftijd');
+// const taskInput2 = document.getElementById('image');
+// const taskInput3 = document.getElementById('geslacht');
+// //input Biografie
+// const taskInput4 = document.getElementById('biografie');
+// // input interesses
+// const taskInput5 = document.getElementById('gebruikersnaam');
+//
+// form.addEventListener('submit',runEvent);
+// function runEvent(e){
+//     console.log(`EVENT TYPE: ${e.type}`);
+//
+// //Get Input value
+// console.log(taskInput.value,taskInput1.value,taskInput2.value,taskInput3.value,taskInput4,taskInput5.value);
+// e.preventDefault();
+// }
+//
+// //checkbox
+// const cb = document.querySelector('#checkboxShopen,#checkboxZwemmen,#checkboxUitgaan');
+// console.log(cb.checked); // false
+//
 
 
 //weet niet zeker of ik dit nodig heb
-const voornaam  = document.getElementById("voornaam");
-
-FYSCloud.API.queryDatabase(
-    'SELECT * FROM gebruiker WHERE id = ?', [FYSCloud.Session.get("userId", "Not Found")]
-).then(function (data){
-    voornaam.innerHTML = data[2].naam
-
-}).catch(function (reason){
-    console.log(reason)
-})
+// const voornaam  = document.getElementById("voornaam");
+//
+// FYSCloud.API.queryDatabase(
+//     'SELECT * FROM gebruiker WHERE id = ?', [FYSCloud.Session.get("userId", "Not Found")]
+// ).then(function (data){
+//     voornaam.innerHTML = data[2].naam
+//
+// }).catch(function (reason){
+//     console.log(reason)
+// })
 
 //persoonlijke informatie uit de database
 document.querySelector("#foto-bestand").addEventListener("change",
