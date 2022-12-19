12070b49a9ed732832ed78f7c7540d5b4820d9b3
Aleksandrs Soskolainens <SOSKOLA@ad.hva.nl>
2022-12-19 14:50:41 +0100

    gewerkt aan matching systeem

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -31,30 +31,3 @@ dropdowns.forEach(dropdown => {
 console.log(FYSCloud.Session.get("userId"))
 console.log(FYSCloud.Session.get("email"))
 
-
-
-// CHECKEN OF ER MATCHES ZIJN
-
-function likeButtonClick(){
-    console.log("Huidige gebruikerID van bezochte profiel: " + userId);
-    console.log("Huidige gebruikerID van ingelogde gebruiker: " + FYSCloud.Session.get("userId", "Not Found"));
-
-    FYSCloud.API.queryDatabase(
-        "INSERT INTO `gebruiker_has_gebruiker` SET ingelogde_gebruiker_id = ?, liked_persoon_id = ?;",
-        [FYSCloud.Session.get("userId", "Not Found"), userId]
-    )
-
-    console.log("Query is gestuurd naar database. (Like systeem)")
-}
-
-FYSCloud.API.queryDatabase(
-    "SELECT * FROM `fys_is104_4_dev`.`gebruiker_has_gebruiker` WHERE ingelogde_gebruiker_id = ? AND liked_persoon_id = ? OR liked_persoon_id = ? AND ingelogde_gebruiker_id = ?;",
-    [FYSCloud.Session.get("userId", "Not Found"), userId, FYSCloud.Session.get("userId", "Not Found"), userId]
-).then(function (data){
-    FYSCloud.API.queryDatabase(
-        "INSERT INTO `matches` SET `gebruiker_id1` = ?, `gebruiker_id2` = ?;",
-        [FYSCloud.Session.get("userId", "Not Found"), userId]
-    )
-    console.log("Match toegevoegd aan matches")
-})
-
