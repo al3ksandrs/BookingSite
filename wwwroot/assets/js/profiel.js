922eeff362d71166241363676e3584b904249d2a
Mohamed <mohamed.embaby@hva.nl>
2022-12-21 17:52:49 +0100

    profiel compleet gemaakt behalve matching historyyy

--- wwwroot/assets/js/profiel.js
+++ wwwroot/assets/js/profiel.js
@@ -15,6 +15,8 @@
 const bioInh = document.querySelector("#biografie_inhoud");
 const pfpInh = document.querySelector("#profielfoto");
 const prlInh = document.querySelector("#profielnaam");
+// Interesses
+const LIMIT_INFO = 5;
 
 // Popup stuff
 const serviceItems = document.querySelector(".service-items");
@@ -61,6 +63,68 @@ function age(dateString){
     let beforeBirth = ((() => {birth.setDate(now.getDate());birth.setMonth(now.getMonth()); return birth.getTime()})() < birth.getTime()) ? 0 : 1;
     return now.getFullYear() - birth.getFullYear() - beforeBirth;
 }
+
+let ulInteresses = document.querySelector("#interesses");
+let count = 0;
+FYSCloud.API.queryDatabase(
+    'SELECT * FROM interesses WHERE (gebruiker_id = ?)', [FYSCloud.Session.get("userId", "Not Found")]
+).then(function (data) {
+    // Loop through all the columns of first row
+    for (let i = 1; i < Object.keys(data[0]).length; i++) {
+        column_name = Object.keys(data[0])[i];
+        if (Object.values(data[0])[i] === 1) {
+            let x = document.createElement("li");
+            x.innerText = column_name;
+            ulInteresses.append(x);
+            count += 1;
+        }
+        if (count === LIMIT_INFO) {break;}
+    }
+}).catch(function (reason){
+    console.log(reason)
+})
+
+let ulSpreekt = document.querySelector("#spreekt");
+let countSpreekt = 0;
+FYSCloud.API.queryDatabase(
+    'SELECT * FROM talen WHERE (gebruiker_id = ?)', [FYSCloud.Session.get("userId", "Not Found")]
+).then(function (data) {
+    // Loop through all the columns of first row
+    for (let i = 1; i < Object.keys(data[0]).length; i++) {
+        column_name = Object.keys(data[0])[i];
+        if (Object.values(data[0])[i] === 1) {
+            let x = document.createElement("li");
+            x.innerText = column_name;
+            ulSpreekt.append(x);
+            countSpreekt += 1;
+        }
+        if (countSpreekt === LIMIT_INFO) {break;}
+    }
+}).catch(function (reason){
+    console.log(reason)
+})
+
+
+let ulReizen = document.querySelector("#reizen");
+let countReis = 0;
+FYSCloud.API.queryDatabase(
+    'SELECT * FROM reis WHERE (gebruiker_id = ?)', [FYSCloud.Session.get("userId", "Not Found")]
+).then(function (data) {
+    // Loop through all the columns of first row
+    for (let i = 1; i < Object.keys(data[0]).length; i++) {
+        column_name = Object.keys(data[0])[i];
+        if (Object.values(data[0])[i] === 1) {
+            let x = document.createElement("li");
+            x.innerText = column_name;
+            ulReizen.append(x);
+            countReis += 1;
+        }
+        if (countReis === LIMIT_INFO) {break;}
+    }
+}).catch(function (reason){
+    console.log(reason)
+})
+
 
 FYSCloud.API.queryDatabase(
     'SELECT * FROM gebruiker WHERE id = ?', [FYSCloud.Session.get("userId", "Not Found")]
