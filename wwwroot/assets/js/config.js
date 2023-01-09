689cb0245151cf5da93dc694a6c1646665da2b9f
Mohamed <mohamed.embaby@hva.nl>
2023-01-09 21:12:11 +0100

    move text voor gematched gefixt en responsive en all

--- wwwroot/assets/js/config.js
+++ wwwroot/assets/js/config.js
@@ -2,5 +2,5 @@ FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is104_4.7xrJee8RutPdAeea",
     database: "fys_is104_4_dev",
-    environment: "mockup"
+    environment: "live"
 });
